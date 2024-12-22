8ce26f4c6d98f67ddd939a43d056ca64a5fe2197
spectreflay <spectreflay@gmail.com>
2024-12-22 12:38:10 +0800

    fixed theme

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,24 +1,34 @@
 import React, { useEffect } from 'react';
 import { useThemeStore } from '../store/ui/themeStore';
 import { useGetUserThemeQuery } from '../store/services/userService';
+import { useGetStaffThemeQuery } from '../store/services/staffService';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store';
 
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const { theme, setTheme } = useThemeStore();
-  const { token } = useSelector((state: RootState) => state.auth);
-  const { data: userTheme, isSuccess } = useGetUserThemeQuery(undefined, {
-    skip: !token,
+  const { token, staff, user } = useSelector((state: RootState) => state.auth);
+  
+  // Only fetch user theme if we have a regular user
+  const { data: userTheme, isSuccess: userThemeSuccess } = useGetUserThemeQuery(undefined, {
+    skip: !token || !!staff,
   });
 
-  // Set theme from user preferences only when logged in
+  // Only fetch staff theme if we have a staff user
+  const { data: staffTheme, isSuccess: staffThemeSuccess } = useGetStaffThemeQuery(staff?._id || '', {
+    skip: !token || !staff,
+  });
+
+  // Set theme from user preferences when logged in
   useEffect(() => {
-    if (token && isSuccess && userTheme) {
-      setTheme(userTheme.themePreference as 'light' | 'dark' | 'green' | 'indigo');
-    } else if (!token) {
+    if (!token) {
       setTheme('light');
+    } else if (staff && staffThemeSuccess && staffTheme) {
+      setTheme(staffTheme.themePreference as 'light' | 'dark' | 'green' | 'indigo');
+    } else if (user && userThemeSuccess && userTheme) {
+      setTheme(userTheme.themePreference as 'light' | 'dark' | 'green' | 'indigo');
     }
-  }, [token, isSuccess, userTheme, setTheme]);
+  }, [token, staff, user, staffTheme, userTheme, staffThemeSuccess, userThemeSuccess, setTheme]);
 
   // Apply theme to document
   useEffect(() => {
